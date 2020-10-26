30de5452408e050ef011817acc33467050895c1e
Swen Mun <longfinfunnel@gmail.com>
2020-10-26 11:46:17 +0900

    Separate create-activation-keys from create

--- .Lib9c.Tools/SubCommand/Genesis.cs
+++ .Lib9c.Tools/SubCommand/Genesis.cs
@@ -1,24 +1,51 @@
 using Cocona;
 using Libplanet;
-using Libplanet.Action;
 using Libplanet.Blocks;
 using Libplanet.Crypto;
 using Nekoyume;
 using Nekoyume.Action;
 using Nekoyume.Model;
 using Nekoyume.Model.State;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
+using NCAction = Libplanet.Action.PolymorphicAction<Nekoyume.Action.ActionBase>;
 
 namespace Lib9c.Tools.SubCommand
 {
     public class Genesis
     {
+        [Command(Description = "Convert key files to pendings")]
+        public void ConvertPending(string activationKeys)
+        {
+            foreach (string line in File.ReadAllLines(activationKeys))
+            {
+                var ak = ActivationKey.Decode(line);
+                var pub = ak.PrivateKey.PublicKey;
+                Console.WriteLine($"{ByteUtil.Hex(pub.Format(true))}/{ByteUtil.Hex(pub.ToAddress().ToByteArray())}");
+            }
+        }
+
+        [Command(Description = "Create activation keys")]
+        public void CreateActivationKeys(
+            [Argument(Description = "Number of keys to create")] int keyCount
+        )
+        {
+            Utils.CreateActivationKey(
+                out List<PendingActivationState> _,
+                out List<ActivationKey> activationKeys,
+                (uint)keyCount);
+            foreach (ActivationKey keys in activationKeys)
+            {
+                Console.WriteLine(keys.Encode());
+            }
+        }
+
         [Command(Description = "Create a new genesis block.")]
         public void Create(
-            [Option("private-key", new[]{ 'p' }, Description = "Hex encoded private key for gensis block")]
+            [Option("private-key", new[]{ 'k' }, Description = "Hex encoded private key for gensis block")]
             string privateKeyHex,
             [Option('g', Description = "/path/to/nekoyume-unity/nekoyume/Assets/AddressableAssets/TableCSV")]
             string gameConfigDir,
@@ -33,14 +60,34 @@ namespace Lib9c.Tools.SubCommand
             [Option('m', Description = "Config path to create AuthorizedMinersState")]
             string authorizedMinerConfigPath = null,
             [Option('c', Description = "Path of a plain text file containing names for credits.")]
-            string creditsPath = null
+            string creditsPath = null,
+            [Option("pending-activations", new[] { 'p' }, Description = "Path of a plain text file containing pending activations")]
+            string pendingActivationsPath = null
         )
         {
+            List<PendingActivationState> pendingActivationStates;
+            if (pendingActivationsPath is null)
+            {
+                Utils.CreateActivationKey(
+                    out pendingActivationStates,
+                    out List<ActivationKey> activationKeys,
+                    activationKeyCount);
+                ExportKeys(activationKeys, "keys.txt");
+            }
+            else
+            {
+                pendingActivationStates = new List<PendingActivationState>();
+                foreach (string line in File.ReadAllLines(pendingActivationsPath))
+                {
+                    var parts = line.Split("/");
+                    pendingActivationStates.Add(new PendingActivationState(
+                        ByteUtil.ParseHex(parts[1]),
+                        new PublicKey(ByteUtil.ParseHex(parts[0]))
+                    ));
+                }
+            }
+
             Dictionary<string, string> tableSheets = Utils.ImportSheets(gameConfigDir);
-            Utils.CreateActivationKey(
-                out List<PendingActivationState> pendingActivationStates,
-                out List<ActivationKey> activationKeys,
-                activationKeyCount);
             GoldDistribution[] goldDistributions = GoldDistribution
                 .LoadInDescendingEndBlockOrder(goldDistributedPath);
 
@@ -69,7 +116,6 @@ namespace Lib9c.Tools.SubCommand
             );
 
             ExportBlock(block, "genesis-block");
-            ExportKeys(activationKeys, "keys.txt");
         }
 
         private static void ExportBlock(Block<PolymorphicAction<ActionBase>> block, string path)
