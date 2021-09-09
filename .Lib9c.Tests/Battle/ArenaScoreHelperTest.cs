e7d3747913c683df6ba981f9b808dca55ea75c02
Han Changsoo <hanc1208@gmail.com>
2021-09-09 22:54:23 +0900

    Update ArenaScoreHelperTest.cs

--- .Lib9c.Tests/Battle/ArenaScoreHelperTest.cs
+++ .Lib9c.Tests/Battle/ArenaScoreHelperTest.cs
@@ -11,27 +11,29 @@ namespace Lib9c.Tests
         public void GetScore()
         {
             var challengerRating = 10000;
-            var defenderRating = challengerRating + Math.Abs(ArenaScoreHelper.DifferLowerLimit);
-            var score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Win);
-            Assert.Equal(ArenaScoreHelper.WinScoreMax, score);
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Lose);
-            Assert.Equal(ArenaScoreHelper.LoseScoreMin, score);
-            defenderRating++;
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Win);
-            Assert.Equal(ArenaScoreHelper.WinScoreMax, score);
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Lose);
-            Assert.Equal(ArenaScoreHelper.LoseScoreMin, score);
-
-            defenderRating = challengerRating - Math.Abs(ArenaScoreHelper.DifferUpperLimit);
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Win);
-            Assert.Equal(ArenaScoreHelper.WinScoreMin, score);
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Lose);
-            Assert.Equal(ArenaScoreHelper.LoseScoreMax, score);
-            defenderRating--;
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Win);
-            Assert.Equal(ArenaScoreHelper.WinScoreMin, score);
-            score = ArenaScoreHelper.GetScore(challengerRating, defenderRating, BattleLog.Result.Lose);
-            Assert.Equal(ArenaScoreHelper.LoseScoreMax, score);
+            var defenderRating = 9000;
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 11000, BattleLog.Result.Win), 60);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 11000, BattleLog.Result.Lose), -30);
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 11001, BattleLog.Result.Win), 60);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 11001, BattleLog.Result.Lose), -30);
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9000, BattleLog.Result.Win), 1);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9000, BattleLog.Result.Lose), -30);
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 8999, BattleLog.Result.Win), 1);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 8999, BattleLog.Result.Lose), -30);
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9900, BattleLog.Result.Win), 8);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9901, BattleLog.Result.Win), 15);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 10000, BattleLog.Result.Win), 15);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 10001, BattleLog.Result.Win), 20);
+            
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9800, BattleLog.Result.Lose), -20);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 9801, BattleLog.Result.Lose), -10);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 10000, BattleLog.Result.Lose), -10);
+            Assert.Equal(ArenaScoreHelper.GetScore(10000, 10001, BattleLog.Result.Lose), -8);
         }
     }
 }
