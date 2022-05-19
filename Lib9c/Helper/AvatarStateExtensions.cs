82bb492596bf85deb80f014e58eeaeece959c67f
hskim881028 <andykim881028@gmail.com>
2022-05-19 21:49:19 +0900

    revert

--- Lib9c/Helper/AvatarStateExtensions.cs
+++ Lib9c/Helper/AvatarStateExtensions.cs
@@ -60,7 +60,7 @@ namespace Nekoyume.Helper
                 var maxExp = row.Exp + row.ExpNeed;
                 var remainExp = maxExp - currentExp;
                 var stageExp = StageRewardExpHelper.GetExp(currentLevel, stageId);
-                var requiredCount = (int)Math.Ceiling(remainExp / (decimal)stageExp);
+                var requiredCount = (int)Math.Ceiling(remainExp / (double)stageExp);
                 if (remainCount - requiredCount > 0) // level up
                 {
                     currentExp += stageExp * requiredCount;
