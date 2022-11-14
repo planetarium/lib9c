4ac7bca1e28d88b7df22281ea246e8770595e4a7
Hyun Seungmin <boscohyun@gmail.com>
2022-11-14 18:06:11 +0900

    Clean up minor

--- Lib9c/Policy/BlockPolicySource.cs
+++ Lib9c/Policy/BlockPolicySource.cs
@@ -88,15 +88,15 @@ namespace Nekoyume.BlockChain.Policy
 
         public const long V100310ObsoleteIndex = 5_300_000;
 
-        // NOTE:
-        // V100311: ArenaSheet-2,6,Championship,5398001(start arena block index)
-        public const long V100320ObsoleteIndex = 5_398_001;
-
         public const long PermissionedMiningStartIndex = 2_225_500;
 
         public const long V100301ExecutedBlockIndex = 5_048_399L;
 
         public const long V100310ExecutedBlockIndex = 5_217_577L;
+
+        // NOTE:
+        // V100311: ArenaSheet-2,6,Championship,5398001(start arena block index)
+        public const long V100320ObsoleteIndex = 5_398_001;
 
         public static readonly TimeSpan BlockInterval = TimeSpan.FromSeconds(8);
 
