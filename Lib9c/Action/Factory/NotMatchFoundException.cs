1c28934c074f25927cea2911834457bb926e299e
Hyun Seungmin <boscohyun@gmail.com>
2023-01-30 21:12:39 +0900

    Refactor `NotMatchFoundException`

--- Lib9c/Action/Factory/NotMatchFoundException.cs
+++ Lib9c/Action/Factory/NotMatchFoundException.cs
@@ -5,24 +5,30 @@ namespace Nekoyume.Action.Factory
 {
     public class NotMatchFoundException : Exception
     {
-        public NotMatchFoundException(string message) : base(message)
-        {
-        }
-
-        public NotMatchFoundException(string message, Exception innerException) :
+        public NotMatchFoundException(string message, Exception innerException = null) :
             base(message, innerException)
         {
         }
 
-        public NotMatchFoundException(Type type, long blockIndex) :
-            base($"There is no matching action for {type.Name}" +
-                 $" at block index {blockIndex}.")
+        public NotMatchFoundException(
+            Type type,
+            long blockIndex,
+            Exception innerException = null) :
+            this(
+                $"There is no matching action for {type.Name}" +
+                $" at block index {blockIndex}.",
+                innerException)
         {
         }
 
-        public NotMatchFoundException(Type type, string actionType) :
-            base($"There is no matching action for {type.Name}" +
-                 $" with action type \"{actionType}\".")
+        public NotMatchFoundException(
+            Type type,
+            string actionType,
+            Exception innerException = null) :
+            this(
+                $"There is no matching action for {type.Name}" +
+                $" with action type \"{actionType}\".",
+                innerException)
         {
         }
     }
