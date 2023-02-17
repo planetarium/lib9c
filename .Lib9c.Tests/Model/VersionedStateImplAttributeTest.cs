c523d60f72bd5d29c4678b7cfc4c30677302527e
Hyun Seungmin <boscohyun@gmail.com>
2023-02-17 19:46:54 +0900

    Introduce snapshot test for versioned states

--- .Lib9c.Tests/Model/VersionedStateImplAttributeTest.cs
+++ .Lib9c.Tests/Model/VersionedStateImplAttributeTest.cs
@@ -2,10 +2,13 @@ namespace Lib9c.Tests.Model
 {
     using System;
     using System.Linq;
+    using Bencodex.Types;
     using Lib9c.Tests.Fixture.States;
     using Nekoyume.Model;
     using Nekoyume.Model.State;
     using Serilog;
+    using Snapshooter;
+    using Snapshooter.Xunit;
     using Xunit;
     using Xunit.Abstractions;
 
@@ -63,6 +66,37 @@ namespace Lib9c.Tests.Model
             }
 
             Assert.False(hasError);
+        }
+
+        [Fact]
+        public void SnapshotTest()
+        {
+            var tuples = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Select(type =>
+                {
+                    var attr = type.GetCustomAttributes(true)
+                        .OfType<VersionedStateImplAttribute>()
+                        .FirstOrDefault();
+                    return (type, attr);
+                })
+                .Where(tuple => tuple.attr is { })
+                .Select(tuple =>
+                {
+                    var state = (IState)Activator.CreateInstance(tuple.type);
+                    return (
+                        type: tuple.type,
+                        value: state?.Serialize() ?? Null.Value,
+                        versionedStateType: tuple.attr.Type);
+                })
+                .ToArray();
+            foreach (var (type, value, versionedStateType) in tuples)
+            {
+                // Match with implementation type.
+                Snapshot.Match(value, SnapshotNameExtension.Create(type.FullName));
+                // Match with versioned state interface type.
+                Snapshot.Match(value, SnapshotNameExtension.Create(versionedStateType.FullName));
+            }
         }
     }
 }
