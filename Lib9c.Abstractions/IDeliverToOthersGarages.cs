ab0630e44698de84d2d1025bc110fa6e5f511083
Hyun Seungmin <boscohyun@gmail.com>
2023-06-01 11:39:01 +0900

    Introduce DeliverToOthersGarages

---
+++ Lib9c.Abstractions/IDeliverToOthersGarages.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Security.Cryptography;
+using Libplanet;
+using Libplanet.Assets;
+
+namespace Lib9c.Abstractions
+{
+    public interface IDeliverToOthersGarages
+    {
+        Address RecipientAgentAddr { get; }
+        IOrderedEnumerable<FungibleAssetValue>? FungibleAssetValues { get; }
+        IOrderedEnumerable<(HashDigest<SHA256> fungibleId, int count)>? FungibleIdAndCounts { get; }
+    }
+}
