21b49cc84cdecd16812c249bdeb82c8fca0c14bc
Yang Chun Ung <qooraven@gmail.com>
2023-06-16 18:34:17 +0900

    Add plegeAddress in plainValue for improve speed

--- .Lib9c.Tests/Action/CreatePledgeTest.cs
+++ .Lib9c.Tests/Action/CreatePledgeTest.cs
@@ -7,27 +7,45 @@ namespace Lib9c.Tests.Action
     using Libplanet.State;
     using Nekoyume;
     using Nekoyume.Action;
+    using Serilog;
     using Xunit;
+    using Xunit.Abstractions;
 
     public class CreatePledgeTest
     {
+        public CreatePledgeTest(ITestOutputHelper outputHelper)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(outputHelper)
+                .CreateLogger();
+        }
+
         [Fact]
         public void Execute()
         {
             var patronAddress = new PrivateKey().ToAddress();
             var mead = Currencies.Mead;
             var agentAddress = new PrivateKey().ToAddress();
+            var pledgeAddress = agentAddress.GetPledgeAddress();
             IAccountStateDelta states = new State()
-                .MintAsset(patronAddress, 4 * mead);
+                .MintAsset(patronAddress, 4 * 500 * mead);
+
+            var agentAddresses = new List<(Address, Address)>
+            {
+                (agentAddress, pledgeAddress),
+            };
+            for (int i = 0; i < 499; i++)
+            {
+                var address = new PrivateKey().ToAddress();
+                agentAddresses.Add((address, address.GetPledgeAddress()));
+            }
 
             var action = new CreatePledge
             {
                 PatronAddress = patronAddress,
                 Mead = RequestPledge.RefillMead,
-                AgentAddresses = new List<Address>
-                {
-                    agentAddress,
-                },
+                AgentAddresses = agentAddresses,
             };
 
             var nextState = action.Execute(new ActionContext
