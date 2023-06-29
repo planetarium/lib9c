77caea7842385c99f885c52f236e431b79775c8d
Swen Mun <longfinfunnel@gmail.com>
2023-06-29 09:38:20 +0900

    Disable Mead requring policy

--- Lib9c/MeadConfig.cs
+++ Lib9c/MeadConfig.cs
@@ -5,6 +5,6 @@ namespace Nekoyume
     public static class MeadConfig
     {
         // mead patron address
-        public static readonly Address PatronAddress = new Address("0xc64c7cBf29BF062acC26024D5b9D1648E8f8D2e1");
+        public static readonly Address PatronAddress = default;
     }
 }
