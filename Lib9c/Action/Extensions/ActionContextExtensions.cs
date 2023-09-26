5710b2e88d4ffd6fcdb167179ec4b8ed178c126e
ilgyu <ilgyu.yi@gmail.com>
2023-09-26 20:12:03 +0900

    refactor: Move Extensions
    Co-authored-by: Chanhyuck Ko <limeelbee@gmail.com>
    Co-authored-by: Lee, Suho <iam@suho.io>

--- Lib9c/Action/Extensions/ActionContextExtensions.cs
+++ Lib9c/Action/Extensions/ActionContextExtensions.cs
@@ -1,13 +1,15 @@
 using Libplanet.Action;
+using Libplanet.Action.State;
 using Libplanet.Crypto;
+using Nekoyume.Module;
 
-namespace Nekoyume.Action
+namespace Nekoyume.Action.Extensions
 {
     public static class ActionContextExtensions
     {
         public static bool IsMainNet(this IActionContext context)
         {
-            var goldCurrency = context.PreviousState.GetGoldCurrency();
+            var goldCurrency = LegacyModule.GetGoldCurrency(context.PreviousState);
             return goldCurrency.Minters
                        .Contains(new Address("47d082a115c63e7b58b1532d20e631538eafadde"))
                    && goldCurrency.Ticker == "NCG"
