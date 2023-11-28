6fe9e9ab59b048253a8b1ca952304f5fe28162a9
sonohoshi <dev.sonohoshi@gmail.com>
2023-11-28 19:32:19 +0900

    make BattleStatus.Tick and Buff.Stun

---
+++ Lib9c/Model/Buff/Stun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nekoyume.Model.Buff
+{
+    [Serializable]
+    public class Stun : Buff
+    {
+        public Stun(BuffInfo buffInfo) : base(buffInfo)
+        {
+
+        }
+
+        protected Stun(Buff value) : base(value)
+        {
+        }
+
+        public override object Clone()
+        {
+            return new Stun(this);
+        }
+    }
+}
