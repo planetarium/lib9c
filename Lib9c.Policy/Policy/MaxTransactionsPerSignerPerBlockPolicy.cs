5fd5e579406eb0a34de56d8ca6a2be30474a6d5f
Suho Lee <iam@suho.io>
2024-02-19 15:15:45 +0900

    test

--- Lib9c.Policy/Policy/MaxTransactionsPerSignerPerBlockPolicy.cs
+++ Lib9c.Policy/Policy/MaxTransactionsPerSignerPerBlockPolicy.cs
@@ -18,21 +18,10 @@ namespace Nekoyume.Blockchain.Policy
             new MaxTransactionsPerSignerPerBlockPolicy(int.MaxValue);
 
         public static IVariableSubPolicy<int> Mainnet =>
-            Default
-                // Note: Introduced to prevent transactions spamming that may result in
-                // the chain grinding to a halt without meaningful state transitions happening.
-                // See https://github.com/planetarium/libplanet/issues/1449.
-                // Issued for v100086.
-                // FIXME: Starting index and value must be finalized accordingly before deployment.
-                .Add(new SpannedSubPolicy<int>(
-                    startIndex: 2_800_001,
-                    value: 4));
+            Default;
 
         // Note: For internal testing.
         public static IVariableSubPolicy<int> Internal =>
-            Default
-                .Add(new SpannedSubPolicy<int>(
-                    startIndex: 2_800_001,
-                    value: 4));
+            Default;
     }
 }
