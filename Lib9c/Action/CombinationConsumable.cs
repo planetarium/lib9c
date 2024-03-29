62c6b62cf5447e6d4e938489d8a4151ec3d26bed
sonohoshi <dev.sonohoshi@gmail.com>
2024-03-29 18:08:17 +0900

    use ActionPointModule at CombinationConsumable

--- Lib9c/Action/CombinationConsumable.cs
+++ Lib9c/Action/CombinationConsumable.cs
@@ -158,16 +158,19 @@ namespace Nekoyume.Action
             // ~Remove Required Materials
 
             // Subtract Required ActionPoint
+            // 2024-03-29 기준: 레시피에 CostActionPoint가 포함된 케이스는 없으나 TableSheets 상태에 의해 동작이 변경될 수 있기에 작성해둔다.
             if (costActionPoint > 0)
             {
-                if (avatarState.actionPoint < costActionPoint)
+                var actionPoint = states.GetActionPoint(avatarAddress);
+                if (actionPoint < costActionPoint)
                 {
                     throw new NotEnoughActionPointException(
-                        $"{addressesHex}Aborted due to insufficient action point: {avatarState.actionPoint} < {costActionPoint}"
+                        $"{addressesHex}Aborted due to insufficient action point: {actionPoint} < {costActionPoint}"
                     );
                 }
 
-                avatarState.actionPoint -= costActionPoint;
+                actionPoint -= costActionPoint;
+                states = states.SetActionPoint(avatarAddress, actionPoint);
             }
             // ~Subtract Required ActionPoint
 
