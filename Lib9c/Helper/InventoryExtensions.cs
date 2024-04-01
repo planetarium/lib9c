3b170dd4ba283ba9367bb38095e369664558face
sonohoshi <dev.sonohoshi@gmail.com>
2024-04-01 14:48:44 +0900

    move AvatarState.UseAp to InventoryExtensions.UseActionPoint
    더 이상 AvatarState에 있을 필요가 없어 역할을 옮겼습니다.

--- Lib9c/Helper/InventoryExtensions.cs
+++ Lib9c/Helper/InventoryExtensions.cs
@@ -1,5 +1,10 @@
 using System.Linq;
+using Libplanet.Action.State;
+using Libplanet.Crypto;
+using Nekoyume.Action;
 using Nekoyume.Model.Item;
+using Nekoyume.Module;
+using Nekoyume.TableData;
 
 namespace Nekoyume.Helper
 {
@@ -24,6 +29,40 @@ namespace Nekoyume.Helper
             }
 
             return GameConfig.DefaultAvatarArmorId;
+        }
+
+        public static IWorld UseActionPoint(
+            this Inventory inventory,
+            Address avatarAddress,
+            int requiredAp,
+            bool chargeAp,
+            MaterialItemSheet materialItemSheet,
+            long blockIndex,
+            IWorld state)
+        {
+            var actionPointState = state.GetActionPoint(avatarAddress);
+            if (actionPointState < requiredAp)
+            {
+                switch (chargeAp)
+                {
+                    case true:
+                        var row = materialItemSheet
+                            .OrderedList!
+                            .First(r => r.ItemSubType == ItemSubType.ApStone);
+                        if (!inventory.RemoveFungibleItem(row.ItemId, blockIndex))
+                        {
+                            throw new NotEnoughMaterialException("not enough ap stone.");
+                        }
+
+                        actionPointState = DailyReward.ActionPointMax;
+                        break;
+                    case false:
+                        throw new NotEnoughActionPointException("");
+                }
+            }
+
+            actionPointState -= requiredAp;
+            return state.SetActionPoint(avatarAddress, actionPointState);
         }
     }
 }
