34ef59f8d11e1d1d490bdd1bd7248c6e4b7e6079
Yang Chun Ung <qooraven@gmail.com>
2024-04-04 15:33:56 +0900

    Fix test

--- .Lib9c.Tests/Model/State/CollectionStateTest.cs
+++ .Lib9c.Tests/Model/State/CollectionStateTest.cs
@@ -15,19 +15,20 @@ namespace Lib9c.Tests.Model.State
                 Ids = new SortedSet<int>
                 {
                     1,
-                    2,
+                    3,
                     1,
+                    2,
                     2,
                 },
             };
 
             var serialized = state.Bencoded;
             var expected = List.Empty
-                .Add(List.Empty.Add(1).Add(2));
+                .Add(List.Empty.Add(1).Add(2).Add(3));
             Assert.Equal(expected, serialized);
 
             var deserialized = new CollectionState(serialized);
-            Assert.Equal(new List<int> { 1, 2, }, deserialized.Ids);
+            Assert.Equal(new SortedSet<int> { 1, 2, 3, }, deserialized.Ids);
         }
     }
 }
