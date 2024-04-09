dd640d3b70d1f06abcb2716af619acf010e20b69
s2quake <han0210@netsgo.com>
2024-04-09 11:14:30 +0900

    refactor: Change decimal places for NCG, ConsensusToken, Share.

--- Lib9c/Action/DPoS/Misc/Asset.cs
+++ Lib9c/Action/DPoS/Misc/Asset.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Libplanet.Types.Assets;
 
 namespace Nekoyume.Action.DPoS.Misc
@@ -8,21 +9,18 @@ namespace Nekoyume.Action.DPoS.Misc
             Currency.Legacy("NCG", 2, null);
 
         public static readonly Currency ConsensusToken =
-            Currency.Uncapped("ConsensusToken", 18, minters: null);
+            Currency.Uncapped("ConsensusToken", 0, minters: null);
 
         public static readonly Currency Share =
-            Currency.Uncapped("Share", 18, minters: null);
+            Currency.Uncapped("Share", 0, minters: null);
 
         public static FungibleAssetValue ConsensusFromGovernance(FungibleAssetValue governanceToken)
-        {
-            return new FungibleAssetValue(
-                ConsensusToken, governanceToken.MajorUnit, governanceToken.MinorUnit);
-        }
+            => FungibleAssetValue.FromRawValue(ConsensusToken, governanceToken.RawValue);
+
+        public static FungibleAssetValue ConsensusFromGovernance(BigInteger amount)
+            => ConsensusFromGovernance(GovernanceToken * amount);
 
         public static FungibleAssetValue GovernanceFromConsensus(FungibleAssetValue consensusToken)
-        {
-            return new FungibleAssetValue(
-                GovernanceToken, consensusToken.MajorUnit, consensusToken.MinorUnit);
-        }
+            => FungibleAssetValue.FromRawValue(GovernanceToken, consensusToken.RawValue);
     }
 }
