bdbf77f1278542b7ddacbdd68fe0831809ad70c1
hyeon <hyeon@planetariumhq.com>
2024-04-12 11:38:50 +0900

    Fix wrong rune level bonus calculating logic
    - Rune level coef in sheet is bp. Devide by 10000
    - Total bonus is multiple of bonus value * total bonus level

--- Lib9c/Helper/RuneHelper.cs
+++ Lib9c/Helper/RuneHelper.cs
@@ -39,7 +39,8 @@ namespace Nekoyume.Helper
         )
         {
             var row = sheet.Values.First(r => r.Rank == rank && r.BossId == bossId);
-            var rewardRow = rewardSheet.OrderedRows.First(r => r.Rank == rank && r.BossId == bossId);
+            var rewardRow =
+                rewardSheet.OrderedRows.First(r => r.Rank == rank && r.BossId == bossId);
             if (rewardRow is WorldBossKillRewardSheet.Row kr)
             {
                 kr.SetRune(random);
@@ -131,7 +132,7 @@ namespace Nekoyume.Helper
         public static int CalculateRuneLevelBonus(AllRuneState allRuneState,
             RuneListSheet runeListSheet, RuneLevelBonusSheet runeLevelBonusSheet)
         {
-            var bonusLevel = 0;
+            var bonusLevel = 0m;
             foreach (var rune in allRuneState.Runes.Values)
             {
                 var runeRow = runeListSheet.Values.FirstOrDefault(row => row.Id == rune.RuneId);
@@ -141,9 +142,11 @@ namespace Nekoyume.Helper
                 }
             }
 
+            bonusLevel /= 10000m;
+
             var bonusRow = runeLevelBonusSheet.Values.OrderByDescending(row => row.RuneLevel)
                 .FirstOrDefault(row => row.RuneLevel <= bonusLevel);
-            return bonusRow?.Bonus ?? 0;
+            return bonusRow?.Bonus * (int)bonusLevel ?? 0;
         }
     }
 }
