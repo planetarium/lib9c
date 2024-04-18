a5514e9b4e353fe6ec27135762007586cb9d83d6
Say Cheong <greymistcube@gmail.com>
2024-04-18 12:48:06 +0900

    Revert "Minor cleanup"
    This reverts commit 01114831e40dd75fcb62809c382639afd182e5ab.

--- Lib9c/Action/RewardGold.cs
+++ Lib9c/Action/RewardGold.cs
@@ -307,10 +307,12 @@ namespace Nekoyume.Action
 
         public static IWorld TransferMead(IActionContext context, IWorld states)
         {
-            var targetAddresses = context.Txs
-                .Where(tx => tx.MaxGasPrice is { } price && price.Currency.Equals(Currencies.Mead))
-                .Select(tx => tx.Signer)
-                .Distinct();
+            var targetAddresses = context.Txs is { } txs
+                ? txs
+                    .Where(tx => tx.MaxGasPrice is { } price && price.Currency.Equals(Currencies.Mead))
+                    .Select(tx => tx.Signer)
+                    .Distinct()
+                : throw new NullReferenceException();
             foreach (var address in targetAddresses)
             {
                 var contractAddress = address.GetPledgeAddress();
