0e5609ec7540d7045219f62c56bb051d9a8525eb
Chanhyuck Ko <limeelbee@gmail.com>
2024-04-19 12:34:58 +0900

    feat: introduce RecordProposer action

---
+++ .Lib9c.Tests/Action/DPoS/Sys/RecordProposerTest.cs
@@ -0,0 +1,40 @@
+﻿namespace Lib9c.Tests.Action.DPoS.Sys
+{
+    using Libplanet.Action.State;
+    using Libplanet.Crypto;
+    using Nekoyume.Action.DPoS;
+    using Nekoyume.Action.DPoS.Misc;
+    using Nekoyume.Action.DPoS.Model;
+    using Nekoyume.Action.DPoS.Sys;
+    using Xunit;
+
+    public class RecordProposerTest : PoSTest
+    {
+        [Fact]
+        public void Execute()
+        {
+            var privateKey1 = new PrivateKey();
+            var privateKey2 = new PrivateKey();
+            var previousProposerInfo = new ProposerInfo(1, privateKey1.Address);
+
+            // Prepare initial state.
+            IWorld initialState = new World(new MockWorldState());
+            initialState = initialState.SetDPoSState(
+                ReservedAddress.ProposerInfo,
+                previousProposerInfo.Bencoded);
+            // Execute the action.
+            initialState = new RecordProposer().Execute(
+                new ActionContext
+                {
+                    PreviousState = initialState,
+                    BlockIndex = 2,
+                    Miner = privateKey2.Address,
+                });
+
+            var proposerInfo =
+                new ProposerInfo(initialState.GetDPoSState(ReservedAddress.ProposerInfo));
+            Assert.Equal(2, proposerInfo.BlockIndex);
+            Assert.True(privateKey2.Address.Equals(proposerInfo.Proposer));
+        }
+    }
+}
---
+++ Lib9c/Action/DPoS/Model/ProposerInfo.cs
@@ -0,0 +1,31 @@
+﻿using Bencodex.Types;
+using Libplanet.Crypto;
+using Nekoyume.Action.DPoS.Util;
+
+namespace Nekoyume.Action.DPoS.Model
+{
+    public class ProposerInfo
+    {
+        public ProposerInfo(long blockIndex, Address proposer)
+        {
+            BlockIndex = blockIndex;
+            Proposer = proposer;
+        }
+
+        public ProposerInfo(IValue bencoded)
+        {
+            var dict = (Dictionary)bencoded;
+            BlockIndex = (Integer)dict["index"];
+            Proposer = dict["proposer"].ToAddress();
+        }
+
+        public long BlockIndex { get; }
+
+        public Address Proposer { get; }
+
+        public IValue Bencoded =>
+            Dictionary.Empty
+                .Add((Text)"index", BlockIndex)
+                .Add((Text)"proposer", Proposer.Bencoded);
+    }
+}
