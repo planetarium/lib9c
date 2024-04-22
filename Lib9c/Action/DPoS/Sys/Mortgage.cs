af24c31dfa38c879c58b343e79b517eacb8e7275
Chanhyuck Ko <limeelbee@gmail.com>
2024-04-22 13:46:26 +0900

    feat: implement gold distribution replacing fee collector

---
+++ Lib9c/Action/DPoS/Sys/Mortgage.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+using Bencodex.Types;
+using Libplanet.Action;
+using Libplanet.Action.State;
+using Libplanet.Types.Assets;
+using Nekoyume.Module;
+
+namespace Nekoyume.Action.DPoS.Sys
+{
+    /// <summary>
+    /// An action for mortgage gas fee for a transaction.
+    /// Should be executed at the beginning of the tx.
+    /// </summary>
+    public sealed class Mortgage : ActionBase
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="Mortgage"/>.
+        /// </summary>
+        public Mortgage()
+        {
+        }
+
+        /// <inheritdoc cref="IAction.PlainValue"/>
+        public override IValue PlainValue => new Bencodex.Types.Boolean(true);
+
+        /// <inheritdoc cref="IAction.LoadPlainValue(IValue)"/>
+        public override void LoadPlainValue(IValue plainValue)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <inheritdoc cref="IAction.Execute(IActionContext)"/>
+        public override IWorld Execute(IActionContext context)
+        {
+            var state = context.PreviousState;
+            if (context.MaxGasPrice is not { } realGasPrice)
+            {
+                return state;
+            }
+
+            var balance = state.GetBalance(context.Signer, realGasPrice.Currency);
+            if (balance < realGasPrice * context.GasLimit())
+            {
+                var msg =
+                    $"The account {context.Signer}'s balance of {realGasPrice.Currency} is " +
+                    "insufficient to pay gas fee: " +
+                    $"{balance} < {realGasPrice * context.GasLimit()}.";
+                throw new InsufficientBalanceException(msg, context.Signer, balance);
+            }
+
+            return state.BurnAsset(
+                context,
+                context.Signer,
+                realGasPrice * context.GasLimit());
+        }
+    }
+}
---
+++ Lib9c/Action/DPoS/Sys/Refund.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Immutable;
+using Bencodex.Types;
+using Libplanet.Action;
+using Libplanet.Action.State;
+using Nekoyume.Module;
+
+namespace Nekoyume.Action.DPoS.Sys
+{
+    /// <summary>
+    /// An action for refund gas fee for a transaction.
+    /// Should be executed at the beginning of the tx.
+    /// </summary>
+    public sealed class Refund : ActionBase
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="Refund"/>.
+        /// </summary>
+        public Refund()
+        {
+        }
+
+        /// <inheritdoc cref="IAction.PlainValue"/>
+        public override IValue PlainValue => new Bencodex.Types.Boolean(true);
+
+        /// <inheritdoc cref="IAction.LoadPlainValue(IValue)"/>
+        public override void LoadPlainValue(IValue plainValue)
+        {
+            // Method intentionally left empty.
+        }
+
+        /// <inheritdoc cref="IAction.Execute(IActionContext)"/>
+        public override IWorld Execute(IActionContext context)
+        {
+            var world = context.PreviousState;
+            if (context.MaxGasPrice is not { Sign: > 0 } realGasPrice)
+            {
+                return world;
+            }
+
+            if (context.GasLimit() - context.GasUsed() <= 0)
+            {
+                return world;
+            }
+
+            return world.MintAsset(
+                context,
+                context.Signer,
+                (context.GasLimit() - context.GasUsed()) * realGasPrice);
+        }
+    }
+}
