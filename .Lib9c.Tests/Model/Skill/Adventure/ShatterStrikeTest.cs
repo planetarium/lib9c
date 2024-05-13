28662e0ec467dcdd6c5f272409528fe72e0aace6
Yang Chun Ung <qooraven@gmail.com>
2024-05-13 15:33:00 +0900

    Divide ShatterStrikeMaxDamage check case

--- .Lib9c.Tests/Model/Skill/Adventure/ShatterStrikeTest.cs
+++ .Lib9c.Tests/Model/Skill/Adventure/ShatterStrikeTest.cs
@@ -29,15 +29,13 @@ namespace Lib9c.Tests.Model.Skill.Adventure
         [InlineData(100, 3700, false, false)]
         [InlineData(100, 100_000, true, true)]
         [InlineData(100, 100_000, true, false)]
-        [InlineData(1_000_000, 100_000, false, true)]
-        [InlineData(1_000_000, 100_000, false, false)]
         public void Use(int enemyHp, int ratioBp, bool expectedEnemyDead, bool copyCharacter)
         {
             var gameConfigState =
                 new GameConfigState((Text)_tableSheets.GameConfigSheet.Serialize());
             Assert.True(
                 _tableSheets.SkillSheet.TryGetValue(700010, out var skillRow)
-            ); // 700011 is ShatterStrike
+            ); // 700010 is ShatterStrike
             var shatterStrike = new ShatterStrike(skillRow, 0, 0, ratioBp, StatType.NONE);
 
             var avatarState = new AvatarState(
@@ -101,6 +99,81 @@ namespace Lib9c.Tests.Model.Skill.Adventure
                 skillInfo.Effect
             );
             Assert.Equal(expectedEnemyDead, skillInfo.IsDead);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Use_MaxDamage(bool copyCharacter)
+        {
+            // 100%
+            var ratioBp = 100_000;
+            const string csv = @"key,value
+shatter_strike_max_damage,1";
+            var gameConfigState =
+                new GameConfigState((Text)csv);
+            Assert.True(
+                _tableSheets.SkillSheet.TryGetValue(700010, out var skillRow)
+            ); // 700010 is ShatterStrike
+            var shatterStrike = new ShatterStrike(skillRow, 0, 0, ratioBp, StatType.NONE);
+
+            var avatarState = new AvatarState(
+                new PrivateKey().Address,
+                new PrivateKey().Address,
+                0,
+                _tableSheets.GetAvatarSheets(),
+                new PrivateKey().Address
+            );
+            var worldRow = _tableSheets.WorldSheet.First;
+            Assert.NotNull(worldRow);
+
+            var random = new TestRandom();
+            var simulator = new StageSimulator(
+                random,
+                avatarState,
+                new List<Guid>(),
+                new AllRuneState(),
+                new RuneSlotState(BattleType.Adventure),
+                new List<Nekoyume.Model.Skill.Skill>(),
+                1,
+                1,
+                _tableSheets.StageSheet[1],
+                _tableSheets.StageWaveSheet[1],
+                false,
+                20,
+                _tableSheets.GetSimulatorSheets(),
+                _tableSheets.EnemySkillSheet,
+                _tableSheets.CostumeStatSheet,
+                StageSimulator.GetWaveRewards(
+                    random,
+                    _tableSheets.StageSheet[1],
+                    _tableSheets.MaterialItemSheet),
+                new List<StatModifier>(),
+                _tableSheets.DeBuffLimitSheet,
+                copyCharacter,
+                shatterStrikeMaxDamage: gameConfigState.ShatterStrikeMaxDamage
+            );
+            var player = new Player(avatarState, simulator);
+            var enemyRow = _tableSheets.CharacterSheet.OrderedList
+                .FirstOrDefault(e => e.Id > 200000);
+            enemyRow.Set(new[]
+            {
+                "201000", "XS", "2", "100", "16", "0", "4", "90", "15", "3.2", "0.64",
+                "0.24", "0", "3.6", "0.6", "0.8", "1.2",
+            });
+            Assert.NotNull(enemyRow);
+
+            var enemy = new Enemy(player, enemyRow, 1);
+            Assert.Equal(100, enemy.HP);
+            // Set def 0 for pure damage check
+            Assert.Equal(0, enemy.DEF);
+
+            player.Targets.Add(enemy);
+            var used = shatterStrike.Use(player, 0, new List<Buff>(), copyCharacter);
+            Assert.NotNull(used);
+            var skillInfo = Assert.Single(used.SkillInfos);
+            Assert.Equal(1, skillInfo.Effect);
+            Assert.False(skillInfo.IsDead);
         }
     }
 }
