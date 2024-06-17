b63a7b5a6adf07e58b21c451fe23a7185dbd8e9f
hyeon <hyeon@planetariumhq.com>
2024-06-17 22:43:57 +0900

    Move UnlockFloorCost from code to CSV

---
+++ Lib9c/TableData/AdventureBoss/AdventureBossUnlockFloorCostSheet.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using static Nekoyume.TableData.TableExtensions;
+
+namespace Nekoyume.TableData.AdventureBoss
+{
+    [Serializable]
+    public class
+        AdventureBossUnlockFloorCostSheet : Sheet<int, AdventureBossUnlockFloorCostSheet.Row>
+    {
+        [Serializable]
+        public class Row : SheetRow<int>
+        {
+            public override int Key => FloorId;
+            public int FloorId;
+            public int GoldenDustPrice;
+            public BigInteger NcgPrice;
+
+            public override void Set(IReadOnlyList<string> fields)
+            {
+                FloorId = TryParseInt(fields[0], out var floorId) ? floorId : 0;
+                GoldenDustPrice = TryParseInt(fields[1], out var dustPrice) ? dustPrice : 0;
+                NcgPrice = TryParseInt(fields[2], out var ncgPrice) ? ncgPrice : 0;
+            }
+        }
+
+        public AdventureBossUnlockFloorCostSheet() : base(nameof(AdventureBossUnlockFloorCostSheet))
+        {
+        }
+    }
+}
