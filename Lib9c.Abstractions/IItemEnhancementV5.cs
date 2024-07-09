3d7d4aaeb0ec444691e6b0d6a8b57e98cbd9457f
Yang Chun Ung <qooraven@gmail.com>
2024-07-09 20:46:10 +0900

    Fix build

--- Lib9c.Abstractions/IItemEnhancementV5.cs
+++ Lib9c.Abstractions/IItemEnhancementV5.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using Libplanet.Crypto;
 
-namespace Lib9c.Abstractions;
-
-public interface IItemEnhancementV5
+namespace Lib9c.Abstractions
 {
-    Guid ItemId { get; }
-    List<Guid> MaterialIds { get; }
-    Address AvatarAddress { get; }
-    int SlotIndex { get; }
-    Dictionary<int, int> Hammers { get; }
+    public interface IItemEnhancementV5
+    {
+        Guid ItemId { get; }
+        List<Guid> MaterialIds { get; }
+        Address AvatarAddress { get; }
+        int SlotIndex { get; }
+        Dictionary<int, int> Hammers { get; }
+    }
 }
