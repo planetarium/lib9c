0935c951a8c0baf379da926497b0d70037a6f0f5
Hyun Seungmin <boscohyun@gmail.com>
2024-07-16 17:41:35 +0900

    introduce `create_avatar` action to @planetarium/lib9c package

---
+++ @planetarium/lib9c/tests/actions/create_avatar.test.ts
@@ -0,0 +1,33 @@
+import { describe } from "vitest";
+import { runTests } from "./common.js";
+import { CreateAvatar } from "../../src/actions/create_avatar.js";
+import { uuidToGuidBytes } from "../../src/actions/common.js";
+
+describe("CreateAvatar", () => {
+    describe("odin", () => {
+        runTests("valid case", [
+            new CreateAvatar({
+                id: uuidToGuidBytes("ae195a5e-b43f-4c6d-8fd3-9f38311a45eb"),
+                index: 0n,
+                hair: 0n,
+                ear: 0n,
+                lens: 0n,
+                tail: 0n,
+                name: "gae-ddong-i",
+            }),
+        ]);
+    });
+    describe("heimdall", () => {
+        runTests("valid case", [
+            new CreateAvatar({
+                id: uuidToGuidBytes("ae195a5e-b43f-4c6d-8fd3-9f38311a45eb"),
+                index: 0n,
+                hair: 0n,
+                ear: 0n,
+                lens: 0n,
+                tail: 0n,
+                name: "gae-ddong-i",
+            }),
+        ]);
+    });
+});
