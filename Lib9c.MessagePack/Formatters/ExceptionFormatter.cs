7f2b05705c72228d8997ebaf3bb2ee00ea1d26dd
sky1045 <ghdwlgud4545@gmail.com>
2024-07-25 18:36:35 +0900

    Fix ExceptionFormatter not to use BinaryFormatter

--- Lib9c.MessagePack/Formatters/ExceptionFormatter.cs
+++ Lib9c.MessagePack/Formatters/ExceptionFormatter.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Buffers;
 using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using MessagePack;
 using MessagePack.Formatters;
@@ -19,14 +21,18 @@ namespace Lib9c.Formatters
                 writer.WriteNil();
                 return;
             }
-            var formatter = new BinaryFormatter();
-            using (var stream = new MemoryStream())
+            var info = new SerializationInfo(typeof(T), new FormatterConverter());
+            value.GetObjectData(info, new StreamingContext(StreamingContextStates.All));
+
+            writer.WriteMapHeader(info.MemberCount + 1);
+            writer.Write("ExceptionType");
+            writer.Write(value.GetType().AssemblyQualifiedName);
+
+            foreach (SerializationEntry entry in info)
             {
-#pragma warning disable SYSLIB0011
-                formatter.Serialize(stream, value);
-#pragma warning restore SYSLIB0011
-                var bytes = stream.ToArray();
-                writer.Write(bytes);
+                writer.Write(entry.Name);
+                writer.Write(entry.ObjectType.AssemblyQualifiedName);
+                MessagePackSerializer.Serialize(entry.ObjectType, ref writer, entry.Value, options);
             }
         }
 
@@ -39,15 +45,79 @@ namespace Lib9c.Formatters
             }
 
             options.Security.DepthStep(ref reader);
-            var formatter = new BinaryFormatter();
-            byte[] bytes = reader.ReadBytes()?.ToArray()
-                ?? throw new MessagePackSerializationException();
-            using (var stream = new MemoryStream(bytes))
+            var count = reader.ReadMapHeader();
+
+            var info = new SerializationInfo(typeof(T), new FormatterConverter());
+            string? typeName = null;
+
+            for (int i = 0; i < count; i++)
             {
-#pragma warning disable SYSLIB0011
-                return (T)formatter.Deserialize(stream);
-#pragma warning restore SYSLIB0011
+                var name = reader.ReadString();
+                if (name == "ExceptionType")
+                {
+                    typeName = reader.ReadString();
+                }
+                else
+                {
+                    var type = Type.GetType(reader.ReadString());
+                    var value = MessagePackSerializer.Deserialize(type, ref reader, options);
+                    info.AddValue(name, value);
+                }
             }
+
+            if (typeName == null)
+            {
+                throw new MessagePackSerializationException("Exception type information is missing.");
+            }
+
+            var exceptionType = Type.GetType(typeName);
+            if (exceptionType == null)
+            {
+                throw new MessagePackSerializationException($"Exception type '{typeName}' not found.");
+            }
+
+            var ctor = exceptionType.GetConstructor(
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+                null,
+                new Type[] { typeof(SerializationInfo), typeof(StreamingContext) },
+                null
+            );
+
+            if (ctor != null)
+            {
+                return (T)ctor.Invoke(new object[] { info, new StreamingContext(StreamingContextStates.All) });
+            }
+
+            var message = info.GetString("Message");
+            var innerException = (Exception?)info.GetValue("InnerException", typeof(Exception));
+
+            T? exception;
+            var constructorWithInnerException = exceptionType.GetConstructor(new[] { typeof(string), typeof(Exception) });
+            if (constructorWithInnerException != null)
+            {
+                exception = (T)constructorWithInnerException.Invoke(new object[] { message!, innerException! });
+            }
+            else
+            {
+                var constructorWithMessage = exceptionType.GetConstructor(new[] { typeof(string) });
+                if (constructorWithMessage != null)
+                {
+                    exception = (T)constructorWithMessage.Invoke(new object[] { message! });
+                }
+                else
+                {
+                    exception = (T?)Activator.CreateInstance(exceptionType);
+                }
+            }
+
+            var stackTrace = info.GetString("StackTraceString");
+            if (!string.IsNullOrEmpty(stackTrace))
+            {
+                var stackTraceField = typeof(Exception).GetField("_stackTraceString", BindingFlags.NonPublic | BindingFlags.Instance);
+                stackTraceField?.SetValue(exception, stackTrace);
+            }
+
+            return exception;
         }
     }
 }
