3e352275de4dbea2de297f758cf1ad74aef7f718
tyrosine1153 <tyrosine1153@gmail.com>
2024-08-06 19:55:09 +0900

    Add Reward Materials fields in GrindingSheet

--- Lib9c/TableData/Crystal/CrystalEquipmentGrindingSheet.cs
+++ Lib9c/TableData/Crystal/CrystalEquipmentGrindingSheet.cs
@@ -11,12 +11,23 @@ namespace Nekoyume.TableData.Crystal
             public int ItemId;
             public int EnchantBaseId;
             public int CRYSTAL;
+            public List<(int materialId, int count)> RewardMaterials = new();
 
             public override void Set(IReadOnlyList<string> fields)
             {
                 ItemId = ParseInt(fields[0]);
                 EnchantBaseId = ParseInt(fields[1]);
                 CRYSTAL = ParseInt(fields[2]);
+                for (int i = 0; i < 2; i++)
+                {
+                    var offset = i * 2;
+                    if (!TryParseInt(fields[3 + offset], out var materialId) || materialId == 0)
+                    {
+                        continue;
+                    }
+
+                    RewardMaterials.Add((materialId, ParseInt(fields[4 + offset])));
+                }
             }
         }
 
