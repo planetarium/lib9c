c0542450e1868c6c8808346756ccf15f4e0c924b
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-08-12 17:30:41 +0900

    apply UnlockCombinationSlotCostSheet to UnlockCombinationSlotAction

--- Lib9c/Action/UnlockCombinationSlot.cs
+++ Lib9c/Action/UnlockCombinationSlot.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Bencodex.Types;
+using Lib9c;
 using Libplanet.Action;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
@@ -18,6 +20,10 @@ namespace Nekoyume.Action
     public class UnlockCombinationSlot : GameAction
     {
         private const string TypeIdentifier = "unlock_combination_slot";
+        
+        // TODO: 별도 파일로 분리
+        public const int GoldenDustId = 600201;
+        public const int RubyDustId = 600202;
         
         public Address AvatarAddress;
         public int SlotIndex;
@@ -40,11 +46,11 @@ namespace Nekoyume.Action
         {
             context.UseGas(1);
             var states = context.PreviousState;
-            // TODO: cost sheet
             var sheets = states.GetSheets(
                 sheetTypes: new[]
                 {
-                    typeof(ArenaSheet),
+                    typeof(MaterialItemSheet),
+                    typeof(UnlockCombinationSlotCostSheet),
                 });
 
             if (!CombinationSlotState.ValidateSlotIndex(SlotIndex))
@@ -60,13 +66,86 @@ namespace Nekoyume.Action
             }
 
             var feeStoreAddress = GetFeeStoreAddress(states, context.BlockIndex);
-            // 코스트 계산
+            var costSheet = sheets.GetSheet<UnlockCombinationSlotCostSheet>();
 
+            if (!costSheet.ContainsKey(SlotIndex))
+            {
+                throw new InvalidSlotIndexException($"[{nameof(UnlockRuneSlot)}] Index On Sheet : {SlotIndex}");
+            }
+            
+            var price = costSheet[SlotIndex];
+            var agentAddress = states.GetAvatarState(AvatarAddress).agentAddress;
+
+            // Use Crystal
+            if (price.CrystalPrice > 0)
+            {
+                var currency = Currencies.Crystal;
+                var balance = states.GetBalance(agentAddress, currency);
+                var crystalPrice = price.CrystalPrice * currency;
+
+                if (balance < price.CrystalPrice * currency)
+                {
+                    throw new InsufficientBalanceException($"{balance} is less than {crystalPrice}",
+                        agentAddress, balance
+                    );
+                }
+
+                states = states.TransferAsset(context, agentAddress, feeStoreAddress, crystalPrice);
+            }
+            
+            // Use GoldenDust
+            if (price.GoldenDustPrice > 0)
+            {
+                var materialSheet = sheets.GetSheet<MaterialItemSheet>();
+                var material = materialSheet.OrderedList.First(m => m.Id == GoldenDustId);
+                
+                var inventory = states.GetInventoryV2(AvatarAddress);
+                if (!inventory.RemoveFungibleItem(material.ItemId, context.BlockIndex,
+                    price.GoldenDustPrice))
+                {
+                    throw new NotEnoughMaterialException(
+                        $"Not enough golden dust to open slot: needs {price.GoldenDustPrice}");
+                }
+                
+                states = states.SetInventory(AvatarAddress, inventory);
+            }
+            
+            // Use RubyDust
+            if (price.RubyDustPrice > 0)
+            {
+                var materialSheet = sheets.GetSheet<MaterialItemSheet>();
+                var material = materialSheet.OrderedList.First(m => m.Id == RubyDustId);
+                
+                var inventory = states.GetInventoryV2(AvatarAddress);
+                if (!inventory.RemoveFungibleItem(material.ItemId, context.BlockIndex,
+                    price.RubyDustPrice))
+                {
+                    throw new NotEnoughMaterialException(
+                        $"Not enough ruby dust to open slot: needs {price.RubyDustPrice}");
+                }
+                
+                states = states.SetInventory(AvatarAddress, inventory);
+            }
+            
+            // Use NCG
+            if (price.NcgPrice > 0)
+            {
+                var currency = states.GetGoldCurrency();
+                var balance = states.GetBalance(agentAddress, currency);
+                var ncgPrice = price.NcgPrice * currency;
+
+                if (balance < price.NcgPrice * currency)
+                {
+                    throw new InsufficientBalanceException($"{balance} is less than {ncgPrice}",
+                        agentAddress, balance
+                    );
+                }
+
+                states = states.TransferAsset(context, agentAddress, feeStoreAddress, ncgPrice);
+            }
+            
             allSlotState.UnlockSlot(AvatarAddress, SlotIndex);
-
-            return states
-                // .TransferAsset(context, context.Signer, feeStoreAddress, cost * currency)
-                .SetCombinationSlotState(AvatarAddress, allSlotState);
+            return states.SetCombinationSlotState(AvatarAddress, allSlotState);
         }
 
         private Address GetFeeStoreAddress(IWorld states, long blockIndex)
