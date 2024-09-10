620dc8a8ec5222e8992e4e071123e665754764c3
Robert <ghdwlgud4545@gmail.com>
2024-09-10 17:20:57 +0900

    Split tag setting

--- Lib9c/Action/HackAndSlash.cs
+++ Lib9c/Action/HackAndSlash.cs
@@ -118,8 +118,9 @@ namespace Nekoyume.Action
             var addressesHex = $"[{signer.ToHex()}, {AvatarAddress.ToHex()}]";
             var started = DateTimeOffset.UtcNow;
             const string source = "HackAndSlash";
-            using var activity = ActivitySource.StartActivity("HackAndSlash")
-                .SetTag("service.name", "Libplanet.Action");
+            using var activity = ActivitySource.StartActivity("HackAndSlash");
+            activity?.SetTag("service.name", "Libplanet.Action");
+
             Log.Verbose("{AddressesHex} {Source} from #{BlockIndex} exec started",
                 addressesHex, source, blockIndex);
 
