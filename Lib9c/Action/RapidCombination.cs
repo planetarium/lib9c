d2c14a461313dc70d1a891f2a5df9b0876ce377c
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2024-09-23 17:09:13 +0900

    fit set avatar state

--- Lib9c/Action/RapidCombination.cs
+++ Lib9c/Action/RapidCombination.cs
@@ -161,7 +161,7 @@ namespace Nekoyume.Action
             var ended = DateTimeOffset.UtcNow;
             Log.Debug("{AddressesHex}RapidCombination Total Executed Time: {Elapsed}", addressesHex, ended - started);
             return states
-                .SetAvatarState(avatarAddress, avatarState)
+                .SetAvatarState(avatarAddress, avatarState, true, true, false, false)
                 .SetCombinationSlotState(avatarAddress, allSlotState);
         }
 
