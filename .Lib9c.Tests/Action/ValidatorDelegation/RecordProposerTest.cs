3abe3300cebc40d859aa40ff638103205fb5f6de
s2quake <han0210@netsgo.com>
2024-09-24 11:05:36 +0900

    test: Add RecordProposer, Update/Release Validator test code

---
+++ .Lib9c.Tests/Action/ValidatorDelegation/RecordProposerTest.cs
@@ -0,0 +1,50 @@
+#nullable enable
+namespace Lib9c.Tests.Action.ValidatorDelegation
+{
+    using System;
+    using Libplanet.Action.State;
+    using Libplanet.Crypto;
+    using Nekoyume.Action.ValidatorDelegation;
+    using Nekoyume.Module.ValidatorDelegation;
+    using Nekoyume.ValidatorDelegation;
+    using Xunit;
+
+    public class RecordProposerTest : ValidatorDelegationTestBase
+    {
+        [Fact]
+        public void Serialization()
+        {
+            var action = new RecordProposer();
+            var plainValue = action.PlainValue;
+
+            var deserialized = new RecordProposer();
+            deserialized.LoadPlainValue(plainValue);
+        }
+
+        [Fact]
+        public void Execute()
+        {
+            // Given
+            var world = World;
+            var minerPrivateKey = new PrivateKey();
+            var blockIndex = (long)Random.Shared.Next(1, 100);
+
+            // When
+            var actionContext = new ActionContext
+            {
+                PreviousState = world,
+                BlockIndex = blockIndex++,
+                Miner = minerPrivateKey.Address,
+            };
+            var recordProposer = new RecordProposer();
+            world = recordProposer.Execute(actionContext);
+
+            // Then
+            var repository = new ValidatorRepository(world, actionContext);
+            var proposerInfo = repository.GetProposerInfo();
+
+            Assert.Equal(blockIndex - 1, proposerInfo.BlockIndex);
+            Assert.Equal(minerPrivateKey.Address, proposerInfo.Proposer);
+        }
+    }
+}
