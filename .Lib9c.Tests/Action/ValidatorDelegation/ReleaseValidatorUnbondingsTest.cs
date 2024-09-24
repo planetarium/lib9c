a898836c6168e73042b115757bd3f804e2cf3f95
s2quake <han0210@netsgo.com>
2024-09-24 11:05:36 +0900

    test: Add RecordProposer, Update/Release Validator test code

---
+++ .Lib9c.Tests/Action/ValidatorDelegation/ReleaseValidatorUnbondingsTest.cs
@@ -0,0 +1,20 @@
+#nullable enable
+namespace Lib9c.Tests.Action.ValidatorDelegation
+{
+    using System;
+    using Nekoyume.Action.ValidatorDelegation;
+    using Xunit;
+
+    public class ReleaseValidatorUnbondingsTest
+    {
+        [Fact]
+        public void Serialization()
+        {
+            var action = new ReleaseValidatorUnbondings();
+            var plainValue = action.PlainValue;
+
+            var deserialized = new ReleaseValidatorUnbondings();
+            deserialized.LoadPlainValue(plainValue);
+        }
+    }
+}
