351cd7c4fe4d74821b1f9b46a8c19bf1ac017bc3
hyeon <hyeon@planetariumhq.com>
2024-09-27 18:40:39 +0900

    Update additional cost logic
    - Move additional cost calculation logic to separated method
    - Check additional cost just before move to next group

--- Lib9c/Helper/CustomCraftHelper.cs
+++ Lib9c/Helper/CustomCraftHelper.cs
@@ -10,6 +10,7 @@ using Nekoyume.Battle;
 using Nekoyume.Model.Item;
 using Nekoyume.TableData;
 using Nekoyume.TableData.CustomEquipmentCraft;
+using static System.Numerics.BigInteger;
 
 namespace Nekoyume.Helper
 {
@@ -30,6 +31,30 @@ namespace Nekoyume.Helper
             return selector.Select(1).First().SelectCp(random);
         }
 
+        public static (BigInteger, IDictionary<int, int>)? CalculateAdditionalCost(
+            int relationship,
+            CustomEquipmentCraftRelationshipSheet relationshipSheet
+        )
+        {
+            var targetRow = relationshipSheet.OrderedList!.FirstOrDefault(
+                row => row.Relationship == relationship + 1
+            );
+
+            if (targetRow is null)
+            {
+                return null;
+            }
+
+            var ncgCost = targetRow.GoldAmount;
+            var itemCosts = new Dictionary<int, int>();
+            foreach (var itemCost in targetRow.MaterialCosts)
+            {
+                itemCosts[itemCost.ItemId] = itemCost.Amount;
+            }
+
+            return (ncgCost, itemCosts);
+        }
+
         public static (BigInteger, IDictionary<int, int>) CalculateCraftCost(
             int iconId,
             int relationship,
@@ -39,7 +64,7 @@ namespace Nekoyume.Helper
             decimal iconCostMultiplier
         )
         {
-            var ncgCost = BigInteger.Zero;
+            var ncgCost = Zero;
             var itemCosts = new Dictionary<int, int>();
             var scrollItemId = materialItemSheet.OrderedList!
                 .First(row => row.ItemSubType == ItemSubType.Scroll).Id;
@@ -60,16 +85,11 @@ namespace Nekoyume.Helper
 
             if (relationshipRow.Relationship == relationship)
             {
-                ncgCost = relationshipRow.GoldAmount;
-                foreach (var itemCost in relationshipRow.MaterialCosts)
-                {
-                    itemCosts[itemCost.ItemId] = itemCost.Amount;
-                }
             }
 
             itemCosts[circleItemId] = (int)Math.Floor(circleCost);
 
-            return (ncgCost, itemCosts.ToImmutableSortedDictionary());
+            return (ncgCost, itemCosts);
         }
     }
 }
