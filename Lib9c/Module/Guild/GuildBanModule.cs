839412679551f8cef97a6041928399ad0ce0dc36
ilgyu <ilgyu.yi@gmail.com>
2024-10-16 21:45:14 +0900

    feat: Remove GuildApplication
    Co-authored-by: s2quake <han0210@netsgo.com>

--- Lib9c/Module/Guild/GuildBanModule.cs
+++ Lib9c/Module/Guild/GuildBanModule.cs
@@ -29,7 +29,11 @@ namespace Nekoyume.Module.Guild
             return false;
         }
 
-        public static void Ban(this GuildRepository repository, GuildAddress guildAddress, AgentAddress signer, AgentAddress target)
+        public static void Ban(
+            this GuildRepository repository,
+            GuildAddress guildAddress,
+            AgentAddress signer,
+            AgentAddress target)
         {
             if (!repository.TryGetGuild(guildAddress, out var guild))
             {
@@ -44,11 +48,6 @@ namespace Nekoyume.Module.Guild
             if (guild.GuildMasterAddress == target)
             {
                 throw new InvalidOperationException("The guild master cannot be banned.");
-            }
-
-            if (repository.TryGetGuildApplication(target, out var guildApplication) && guildApplication.GuildAddress == guildAddress)
-            {
-                repository.RejectGuildApplication(signer, target);
             }
 
             if (repository.GetJoinedGuild(target) == guildAddress)
