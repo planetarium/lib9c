bbd8ad5bd5b89f3b0bebb87f57ccbff108d0dcf3
ilgyu <ilgyu.yi@gmail.com>
2024-10-18 22:26:57 +0900

    fix: Fix action typescript

--- @planetarium/lib9c/tests/actions/make_guild.test.ts
+++ @planetarium/lib9c/tests/actions/make_guild.test.ts
@@ -1,7 +1,12 @@
 import { describe } from "vitest";
 import { MakeGuild } from "../../src/index.js";
 import { runTests } from "./common.js";
+import { validatorAddress } from "./fixtures.js";
 
 describe("MakeGuild", () => {
-  runTests("valid case", [new MakeGuild()]);
+  runTests("valid case", [
+    new MakeGuild({
+      validatorAddress: validatorAddress,
+    }),
+  ]);
 });
