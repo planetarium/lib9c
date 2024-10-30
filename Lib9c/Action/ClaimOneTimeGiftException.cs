fa763cec9c6425d58cbcd3e8947c01cdeb5271f7
tyrosine1153 <tyrosine1153@gmail.com>
2024-10-30 21:47:37 +0900

    create ClaimOneTimeGift(action), OneTimeGiftSheet(sheet)

---
+++ Lib9c/Action/ClaimOneTimeGiftException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Nekoyume.Action
+{
+    [Serializable]
+    public class AlreadyClaimedGiftsException : Exception
+    {
+        public AlreadyClaimedGiftsException()
+        {
+        }
+
+        public AlreadyClaimedGiftsException(string msg) : base(msg)
+        {
+        }
+
+        public AlreadyClaimedGiftsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    public class ClaimableGiftsNotAvailableException : Exception
+    {
+        public ClaimableGiftsNotAvailableException()
+        {
+        }
+
+        public ClaimableGiftsNotAvailableException(string msg) : base(msg)
+        {
+        }
+
+        public ClaimableGiftsNotAvailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
