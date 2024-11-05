2a817b4dcf3ea4c5d5b47cd68a5fcf108e017214
s2quake <han0210@netsgo.com>
2024-11-05 13:12:17 +0900

    refactor: Enable unstaking without a guild

--- Lib9c/Action/ValidatorDelegation/ReleaseValidatorUnbondings.cs
+++ Lib9c/Action/ValidatorDelegation/ReleaseValidatorUnbondings.cs
@@ -14,6 +14,7 @@ using Nekoyume.Module.Guild;
 using Nekoyume.TypedAddress;
 using Nekoyume.Module;
 using Nekoyume.Model.Stake;
+using Lib9c;
 
 namespace Nekoyume.Action.ValidatorDelegation
 {
@@ -68,12 +69,24 @@ namespace Nekoyume.Action.ValidatorDelegation
         {
             var agentAddress = new AgentAddress(address);
             var guildRepository = new GuildRepository(world, context);
+            var goldCurrency = world.GetGoldCurrency();
             if (guildRepository.TryGetGuildParticipant(agentAddress, out var guildParticipant))
             {
                 var guild = guildRepository.GetGuild(guildParticipant.GuildAddress);
-                var goldCurrency = world.GetGoldCurrency();
                 var stakeStateAddress = guildParticipant.DelegationPoolAddress;
                 var gg = world.GetBalance(stakeStateAddress, guild.DelegationCurrency);
+                if (gg.Sign > 0)
+                {
+                    var (ncg, _) = ConvertToGoldCurrency(gg, goldCurrency);
+                    world = world.BurnAsset(context, stakeStateAddress, gg);
+                    world = world.TransferAsset(
+                        context, stakeStateAddress, address, ncg);
+                }
+            }
+            else
+            {
+                var stakeStateAddress = StakeState.DeriveAddress(address);
+                var gg = world.GetBalance(stakeStateAddress, Currencies.GuildGold);
                 if (gg.Sign > 0)
                 {
                     var (ncg, _) = ConvertToGoldCurrency(gg, goldCurrency);
