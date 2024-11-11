d54a4c82fa73daef3cfb0195f92fc6966d1736da
tyrosine1153 <tyrosine1153@gmail.com>
2024-11-11 21:27:11 +0900

    fix exception name

--- Lib9c/Action/AlreadyClaimedGiftsException.cs
+++ Lib9c/Action/AlreadyClaimedGiftsException.cs
@@ -18,20 +18,4 @@ namespace Nekoyume.Action
         {
         }
     }
-
-    [Serializable]
-    public class ClaimableGiftsNotAvailableException : Exception
-    {
-        public ClaimableGiftsNotAvailableException()
-        {
-        }
-
-        public ClaimableGiftsNotAvailableException(string msg) : base(msg)
-        {
-        }
-
-        public ClaimableGiftsNotAvailableException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-    }
 }
