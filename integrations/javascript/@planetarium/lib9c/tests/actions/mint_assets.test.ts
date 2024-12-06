55ded212459e6c10ff96af297bdcdc5e5aba1f86
moreal <dev.moreal@gmail.com>
2024-12-07 00:24:38 +0900

    test(js): create action instance lazily

--- integrations/javascript/@planetarium/lib9c/tests/actions/mint_assets.test.ts
+++ integrations/javascript/@planetarium/lib9c/tests/actions/mint_assets.test.ts
@@ -1,6 +1,5 @@
-import { afterEach } from "node:test";
 import { Decimal } from "decimal.js";
-import { afterAll, beforeEach, describe } from "vitest";
+import { afterAll, afterEach, beforeEach, describe } from "vitest";
 import { MEAD, MintAssets, NCG, fav } from "../../src/index.js";
 import { runTests } from "./common.js";
 import { agentAddress, fungibleIdA } from "./fixtures.js";
@@ -33,58 +32,63 @@ describe("MintAssets", () => {
       mintSpecs: [],
       memo: "memo",
     }),
-    new MintAssets({
-      mintSpecs: [
-        {
-          recipient: agentAddress,
-          amount: fav(MEAD, 100_0000n),
-        },
-      ],
-      memo: "memo",
-    }),
-    new MintAssets({
-      mintSpecs: [
-        {
-          recipient: agentAddress,
-          amount: fav(NCG, 10n),
-        },
-      ],
-      memo: "memo",
-    }),
-    new MintAssets({
-      mintSpecs: [
-        {
-          recipient: agentAddress,
-          fungibleItemId: fungibleIdA,
-          count: 1n,
-        },
-      ],
-      memo: "memo",
-    }),
-    new MintAssets({
-      mintSpecs: [
-        {
-          recipient: agentAddress,
-          fungibleItemId: fungibleIdA,
-          count: 1n,
-        },
-        {
-          recipient: agentAddress,
-          amount: fav(MEAD, 100_0000n),
-        },
-      ],
-      memo: "memo",
-    }),
-    new MintAssets({
-      mintSpecs: [
-        {
-          recipient: agentAddress,
-          fungibleItemId: fungibleIdA,
-          count: 1n,
-          amount: fav(MEAD, 100_0000n),
-        },
-      ],
-      memo: "memo",
-    }),
+    () =>
+      new MintAssets({
+        mintSpecs: [
+          {
+            recipient: agentAddress,
+            amount: fav(MEAD, 100_0000n),
+          },
+        ],
+        memo: "memo",
+      }),
+    () =>
+      new MintAssets({
+        mintSpecs: [
+          {
+            recipient: agentAddress,
+            amount: fav(NCG, 10n),
+          },
+        ],
+        memo: "memo",
+      }),
+    () =>
+      new MintAssets({
+        mintSpecs: [
+          {
+            recipient: agentAddress,
+            fungibleItemId: fungibleIdA,
+            count: 1n,
+          },
+        ],
+        memo: "memo",
+      }),
+    () =>
+      new MintAssets({
+        mintSpecs: [
+          {
+            recipient: agentAddress,
+            fungibleItemId: fungibleIdA,
+            count: 1n,
+          },
+          {
+            recipient: agentAddress,
+            amount: fav(MEAD, 100_0000n),
+          },
+        ],
+        memo: "memo",
+      }),
+    () =>
+      new MintAssets({
+        mintSpecs: [
+          {
+            recipient: agentAddress,
+            fungibleItemId: fungibleIdA,
+            count: 1n,
+            amount: fav(MEAD, 100_0000n),
+          },
+        ],
+        memo: "memo",
+      }),
   ]);
 });
