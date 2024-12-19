c3439b4ff1a30e9ad6ff232119b082e4b23dd0cc
Atralupus <me@atralupus.com>
2024-12-20 00:08:36 +0900

    fix has export path

--- integrations/javascript/@planetarium/lib9c/tests/actions/hack_and_slash.test.ts
+++ integrations/javascript/@planetarium/lib9c/tests/actions/hack_and_slash.test.ts
@@ -1,6 +1,5 @@
 import { describe } from "vitest";
-import { HackAndSlash } from "../../src/actions/hack_and_slash.js";
-import { RuneSlotInfo, uuidToGuidBytes } from "../../src/index.js";
+import { HackAndSlash, RuneSlotInfo, uuidToGuidBytes } from "../../src/index.js";
 import { runTests } from "./common.js";
 import { avatarAddress } from "./fixtures.js";
 
