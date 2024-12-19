0d4f20a55b6c1a40081baaba4a8015ce3757d21f
s2quake <han0210@netsgo.com>
2024-12-19 13:12:38 +0900

    refactor: Remove migration-related test code

--- integrations/javascript/@planetarium/lib9c/tests/actions/migrate_planetarium_guild.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe } from "vitest";
-import { MigratePlanetariumGuild } from "../../src/index.js";
-import { runTests } from "./common.js";
-
-describe("MigratePlanetariumGuild", () => {
-  runTests("valid case", [new MigratePlanetariumGuild()]);
-});
