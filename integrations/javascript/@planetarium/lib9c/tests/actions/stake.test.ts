a08b1bc1ad692d617d4db1b8e4f699a49b23d763
ilgyu <ilgyu.yi@gmail.com>
2025-01-05 18:57:19 +0900

    js: Fix js for stake action modification

--- integrations/javascript/@planetarium/lib9c/tests/actions/stake.test.ts
+++ integrations/javascript/@planetarium/lib9c/tests/actions/stake.test.ts
@@ -1,11 +1,13 @@
 import { describe } from "vitest";
 import { Stake } from "../../src/index.js";
 import { runTests } from "./common.js";
+import { avatarAddress } from "./fixtures.js";
 
 describe("Stake", () => {
   runTests("valid case", [
     new Stake({
       amount: 1n,
+      avatarAddress: avatarAddress,
     }),
   ]);
 });
