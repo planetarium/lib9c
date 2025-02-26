a80715c0ddbfcb531037fdf9f62f1aa914046aeb
ilgyu <ilgyu.yi@gmail.com>
2025-02-26 23:23:44 +0900

    chore: Add documentation

--- Lib9c/ValidatorDelegation/PayMaster.cs
+++ Lib9c/ValidatorDelegation/PayMaster.cs
@@ -6,8 +6,32 @@ using Libplanet.Types.Assets;
 
 namespace Nekoyume.ValidatorDelegation
 {
+    /// <summary>
+    /// PayMaster is a contract that manages the proxy of mortgage and refund of gas.
+    /// </summary>
     public static class PayMaster
     {
+        /// <summary>
+        /// Mortgage gas with the PayMaster.
+        /// </summary>
+        /// <param name="world">
+        /// The <see cref="IWorld"/> where the mortgage happen.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="IActionContext"/> where the mortgage happen.
+        /// </param>
+        /// <param name="address">
+        /// The <see cref="Address"/> of the user to mortgage gas.
+        /// </param>
+        /// <param name="payMaster">
+        /// The <see cref="Address"/> of the paymaster.
+        /// </param>
+        /// <param name="gasToMortgage">
+        /// The <see cref="FungibleAssetValue"/> of gas to mortgage.
+        /// </param>
+        /// <returns>
+        /// The updated <see cref="IWorld"/>.
+        /// </returns>
         public static IWorld Mortgage(
             this IWorld world,
             IActionContext context,
@@ -30,6 +54,24 @@ namespace Nekoyume.ValidatorDelegation
             return world;
         }
 
+        /// <summary>
+        /// Refund gas with the PayMaster.
+        /// </summary>
+        /// <param name="world">
+        /// The <see cref="IWorld"/> where the refund happen.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="IActionContext"/> where the refund happen.
+        /// </param>
+        /// <param name="address">
+        /// The <see cref="Address"/> of the user to refund gas.
+        /// </param>
+        /// <param name="gasToRefund">
+        /// The <see cref="FungibleAssetValue"/> of gas to refund.
+        /// </param>
+        /// <returns>
+        /// The updated <see cref="IWorld"/>.
+        /// </returns>
         public static IWorld Refund(
             this IWorld world,
             IActionContext context,
