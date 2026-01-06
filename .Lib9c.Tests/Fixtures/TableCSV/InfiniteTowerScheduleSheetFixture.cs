a8653530ca35638cefac10a81accd2c113bde0d1
Yang Chun Ung <qooraven@gmail.com>
2026-01-06 19:52:22 +0900

    Fix test

---
+++ .Lib9c.Tests/Fixtures/TableCSV/InfiniteTowerScheduleSheetFixture.cs
@@ -0,0 +1,9 @@
+namespace Lib9c.Tests.Fixtures.TableCSV
+{
+    public static class InfiniteTowerScheduleSheetFixture
+    {
+        public const string Default =
+            @"Id,InfiniteTowerId,StartBlockIndex,EndBlockIndex,DailyFreeTickets,MaxTickets,ResetIntervalBlocks,FloorBegin,FloorEnd
+1,1,0,1000000,3,10,10800,1,100";
+    }
+}
