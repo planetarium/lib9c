69525dd4b58b2647cd31f7dc1cb4d325d2dbf3d3
Yang Chun Ung <qooraven@gmail.com>
2026-03-11 19:42:24 +0900

    Delete test code

--- Lib9c/TableData/SweepRequiredCPSheet.cs
+++ Lib9c/TableData/SweepRequiredCPSheet.cs
@@ -17,35 +17,11 @@ namespace Nekoyume.TableData
                 StageId = ParseInt(fields[0]);
                 RequiredCP = ParseInt(fields[1]);
             }
-
-            public Row CloneWithStageId(int newStageId)
-            {
-                return new Row
-                {
-                    StageId = newStageId,
-                    RequiredCP = RequiredCP,
-                };
-            }
         }
 
         public SweepRequiredCPSheet() : base(nameof(SweepRequiredCPSheet))
         {
         }
 
-        protected override void AddRow(int key, Row value)
-        {
-            base.AddRow(key, value);
-
-            // Extend hard stages as a continuation of normal stages:
-            // stage 451..900 duplicates stage 1..450.
-            if (key >= 1 && key <= 450)
-            {
-                var extendedKey = key + 450;
-                if (!ContainsKey(extendedKey))
-                {
-                    base.AddRow(extendedKey, value.CloneWithStageId(extendedKey));
-                }
-            }
-        }
     }
 }
